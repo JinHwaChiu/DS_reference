8a02c833c936b4830b61c68160fdde0c2fb9ef4e
Jin-Hwa(Joey) <jkmemo2013@gmail.com>
2017-12-11 14:10:22 +0000

    auto working

---
+++ Auto Working/Auto open newtab.bat
@@ -0,0 +1,3 @@
+
+
+C:\Users\MLUSER\Anaconda3\python.exe C:\Users\MLUSER\Desktop\Autoopennewtab.py
---
+++ Auto Working/Autoopennewtab.py
@@ -0,0 +1,15 @@
+#! python3
+# lucky.py - Opens several Google search results.
+
+import requests, sys, webbrowser
+
+
+# Retrieve top search result links.
+
+# Open a browser tab from each link.
+linkElems = ['http://www.cnblogs.com/jinxulin/p/3526542.html',
+             'https://classroom.udacity.com/nanodegrees/nd009/parts/9777e610-ebeb-4c49-b502-88c692489ae3'
+             ,'https://github.com/diyjac/smartcab/blob/master/smartcab.ipynb']
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open(linkElems[i])
